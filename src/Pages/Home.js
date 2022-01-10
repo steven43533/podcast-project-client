b50ddc88a3ba2acda74338d20f301bf41a832179
steven43533 <steven43533@gmail.com>
2022-01-09 21:17:25 -0500

    Added to home page/Search By Podcast button/Search By Genre button/ Search button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,62 @@
+import { useState } from "react"
+
 const Home = (props) => {
-	// const { msgAlert, user } = props
-	console.log('props in home', props)
+
+	const [selectedOption, setSelectedOption] = useState("Search-By-Podcast")
+
+	let handleOptionChange = changeEvent => {
+		setSelectedOption(changeEvent.target.value)
+	}
+	
+	let handleFormSubmit = formSubmitEvent => {
+		formSubmitEvent.preventDefault()
+		console.log("You searched by ", selectedOption);
+	}
 
 	return (
-		<>
-			<h2>HI Page</h2>
-		</>
+		<div className="container">
+			<div className="row mt-5">
+				<div className="col-sm-12">
+
+					<form onSubmit={handleFormSubmit}>
+
+						<div className="form-check">
+							<label>
+								<input
+									type="radio"
+									value="Search-By-Podcast"
+									checked={selectedOption === "Search-By-Podcast"}
+									onChange={handleOptionChange}
+									className="form-check-input"
+								/>
+								Search By Podcast
+							</label>
+						</div>
+
+						<div className="form-check">
+							<label>
+								<input
+									type="radio"
+									value="Search-By-Genre"
+									checked={selectedOption === "Search-By-Genre"}
+									onChange={handleOptionChange}
+									className="form-check-input"
+								/>
+								Search by Genre
+							</label>
+						</div>
+
+						<div className="form-group">
+							<button className="btn btn-primary mt-2" type="submit">
+								Search
+							</button>
+						</div>
+
+					</form>
+
+				</div>
+			</div>
+		</div>
 	)
 }
 
